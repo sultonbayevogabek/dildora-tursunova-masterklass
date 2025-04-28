dd9a793f6b37233f8eae8ffa9f1a52f1cd0ab9c7
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-04-28 23:48:25 +0500

    commit

--- b/js/statistics.js
+++ b/js/statistics.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class Statistics {
-  PROJECT_NAME = 'dildora-tursunova-a'
+  PROJECT_NAME = 'dildora-tursunova-b'
   DB_URL = 'https://webinar-pages-statistics-default-rtdb.firebaseio.com/';
   ENTERED_DB = `${this.DB_URL}${this.PROJECT_NAME}-entered.json`;
   CLICKED_REG_BTN_DB = `${this.DB_URL}${this.PROJECT_NAME}-clicked-reg-btn.json`;
